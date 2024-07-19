f8ee3cb585f372627b1fb7ec2b494b4e1089cab0
Huseyin <78041148+HsynHsyn@users.noreply.github.com>
2024-07-19 23:14:56 +0100

    Update Bat3_sum67.java

--- Bat3_sum67.java
+++ Bat3_sum67.java
@@ -16,22 +16,22 @@ public class Bat3_sum67 {
     private static int sum67(int[] nums) {
 
         int sum = 0;
-        boolean inRange = false; // Başlangıçta 6-7 aralığında değiliz, bu yüzden false
+        boolean inRange = false; 
 
         for (int num : nums) {
             if (num == 6) {
-                inRange = true; // 6'yı gördüğümüzde, 6-7 aralığına gireriz
+                inRange = true; 
             }
             if (!inRange) {
-                sum += num; // Eğer 6-7 aralığında değilsek, sayıyı toplama ekleriz
+                sum += num; 
             }
             if (num == 7 && inRange) {
-                inRange = false; // 7'yi gördüğümüzde ve 6-7 aralığındaysak, aralığı sonlandırırız
+                inRange = false; 
             }
 
         }
 
-        return sum; // Toplamı döndürür
+        return sum; 
     }
 
 
